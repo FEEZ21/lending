3df9616ea30defecf86c95285406e1d8d0fe4718
unknown <feez7700@gmail.com>
2025-06-10 09:52:41 +0300

    Update server to listen on 0.0.0.0 for Render.com

--- backend/src/index.js
+++ backend/src/index.js
@@ -65,6 +65,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '127.0.0.1', () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
